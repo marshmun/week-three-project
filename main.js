2979caded5da343b017fa87766758b722746160f
Marshall Munoz <marshmun@gmail.com>
2017-06-09 14:34:48 -0500

    displays now in the box, and clears throughly

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 var btn = document.querySelectorAll(".btn");
 var clear = document.querySelector('.clear');
 var equals = document.querySelector('.equal')
+var display = document.querySelector('.display')
 var operators = ['/', 'x', '+,' - ''];
 
 
 for (var i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", function (e) {
         var value = e.target.innerHTML;
+        display.innerHTML += value;
         console.log("value:", value);
     });
 }
+
+clear.addEventListener("click", function (e) {
+    display.innerHTML = '';
+});
 
 
