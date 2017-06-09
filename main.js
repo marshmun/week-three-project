4da59613288a39ac62d52a8223f9aaf350c82c64
Marshall Munoz <marshmun@gmail.com>
2017-06-09 10:06:21 -0500

    started down path of java script

---
+++ main.js
@@ -0,0 +1,9 @@
+var mainCalc = document.querySelectorAll('#calculator span');
+var operators = ['+', '-', 'x', '÷'];
+var decimalAdded = false;
+
+for (var index = 0; index < mainCalc.length; index++) {
+    mainCalc[i].onclick = function (e) {
+
+    }
+}
